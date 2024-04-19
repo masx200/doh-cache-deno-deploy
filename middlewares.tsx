b432ed03e87d8679087f5c7ed1fdce44f503134b
masx200 <34191203+masx200@users.noreply.github.com>
2024-04-19 14:35:35 +0800

    添加静态文件

--- middlewares.tsx
+++ middlewares.tsx
@@ -11,6 +11,7 @@ import { logger } from "./logger.tsx";
 import { cache_dns_query_post_and_get_method } from "./cache_dns_query_post.tsx";
 import { loop_detection_prevent_forwarded } from "./loop_detection_prevent_forwarded.ts";
 import { parse_dns_message } from "./parse_dns_message.tsx";
+import { staticHandler } from "./staticHandler.tsx";
 export const middlewares: Middleware[] = [
     error_handler, // 错误处理中间件
     logger, // 日志记录中间件
@@ -21,4 +22,5 @@ export const middlewares: Middleware[] = [
 
     Strict_Transport_Security, // 设置HTTP响应头的中间件
     mainMiddleware, // 主要的中间件
+    staticHandler,
 ];
